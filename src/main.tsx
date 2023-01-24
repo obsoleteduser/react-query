fcbdacccb1a76fbde1a38d65df6d690976f978fa
Tahir <dibirovtahir@gmail.com>
2023-01-24 12:48:09 +0400

    INIT

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App'
 import './index.css'
 
+const client = new  QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+      <QueryClientProvider client={client}>
+            <App></App>
+      </QueryClientProvider>
+   
+ 
 )
